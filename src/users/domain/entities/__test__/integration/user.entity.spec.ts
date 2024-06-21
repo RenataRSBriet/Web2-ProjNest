96e4a2a10bd2177977f753ad9b82b6dda90f5b24
RenataRSBriet <renatarodriguesbriet@gmail.com>
2024-06-20 21:49:24 -0300

    teste integracao nome/email/senha

--- src/users/domain/entities/__test__/integration/user.entity.spec.ts
+++ src/users/domain/entities/__test__/integration/user.entity.spec.ts
@@ -31,6 +31,16 @@ describe('UserEntity integration tests', () => {
         })
 
         
+        it('Should throw an error when creating a user with invalid name - no String', () => {
+            const valid: UserProps = {
+                ...props,
+                name: 10 as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+
+        
         it('Should throw an error when creating a user with invalid name - large', () => {
             const valid: UserProps = {
                 ...props,
@@ -38,6 +48,89 @@ describe('UserEntity integration tests', () => {
             }
             expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
         })
+
+        //email
+
+        it('Should throw an error when creating a user with invalid email - null', () => {
+            const valid: UserProps = {
+                ...props,
+                email: null as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+        
+        it('Should throw an error when creating a user with invalid email - empty', () => {
+            const valid: UserProps = {
+                ...props,
+                email: '' as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+        
+        it('Should throw an error when creating a user with invalid email - no String', () => {
+            const valid: UserProps = {
+                ...props,
+                email: 10 as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+        it('Should throw an error when creating a user with invalid email - no e-mail', () => {
+            const valid: UserProps = {
+                ...props,
+                email: "qualquercoisa" as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+        
+        it('Should throw an error when creating a user with invalid email - large', () => {
+            const valid: UserProps = {
+                ...props,
+                email: 'a'.repeat(256) as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+        //password
+
+        it('Should throw an error when creating a user with invalid password - null', () => {
+            const valid: UserProps = {
+                ...props,
+                password: null as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+        
+        it('Should throw an error when creating a user with invalid password - empty', () => {
+            const valid: UserProps = {
+                ...props,
+                password: '' as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+        
+        it('Should throw an error when creating a user with invalid password - no String', () => {
+            const valid: UserProps = {
+                ...props,
+                password: 10 as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
+
+        
+        it('Should throw an error when creating a user with invalid password - large', () => {
+            const valid: UserProps = {
+                ...props,
+                password: 'a'.repeat(101) as any
+            }
+            expect(() => new UserEntity(valid)).toThrow(EntityValidationError);
+        })
+
     })
 })
 
